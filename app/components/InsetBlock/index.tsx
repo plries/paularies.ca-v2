699e799ff8ba74050cf34db5f3a7ac82a926b3e1
plries <apaulries@gmail.com>
2025-04-04 23:05:03 -0700

    update insetblock border radius

--- app/components/InsetBlock/index.tsx
+++ app/components/InsetBlock/index.tsx
@@ -6,7 +6,7 @@ export const InsetBlock = ({
 }: InsetBlockPropTypes) => {
   return (
     <div
-      className={`bg-greyscale-100 dark:bg-greyscale-900 border-greyscale-950/5 dark:border-greyscale-50/5 col-span-full grid grid-cols-4 gap-3 rounded-3xl border-[1px] p-3 py-6 shadow-[var(--inset-light)] md:grid-cols-8 md:gap-4 md:px-6 md:py-24 lg:grid-cols-12 lg:py-36 dark:shadow-[var(--inset-dark)] ${additionalClasses}`}
+      className={`bg-greyscale-100 dark:bg-greyscale-900 border-greyscale-950/5 dark:border-greyscale-50/5 col-span-full grid grid-cols-4 gap-3 rounded-2xl border-[1px] p-3 py-6 shadow-[var(--inset-light)] md:grid-cols-8 md:gap-4 md:px-6 md:py-24 lg:grid-cols-12 lg:py-36 dark:shadow-[var(--inset-dark)] ${additionalClasses}`}
     >
       {children}
     </div>
