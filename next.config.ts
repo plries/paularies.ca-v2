8094320d81c9919278a0baa8e23c5ce8d6e6a78f
plries <apaulries@gmail.com>
2025-04-14 23:23:37 -0700

    next.config routing

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,20 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: "/",
+        has: [
+          {
+            type: "host",
+            value: "bio.paularies.ca",
+          },
+        ],
+        destination: "/bio",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
