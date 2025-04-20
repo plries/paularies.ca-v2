6e5e34f0c59f5a971ed9285be56cfca738a2af54
plries <apaulries@gmail.com>
2025-04-20 01:07:45 -0700

    updated 404 styling

--- app/const.ts
+++ app/const.ts
@@ -1,4 +1,5 @@
 export const NOT_FOUND_CONST = {
+    HEADING: "error 404",
     DESCRIPTION: "the page you are looking for doesn't exist.",
     BUTTON: "go back",
 };
--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,16 +1,19 @@
 import { hankenGrotesk, instrumentSerif, dmMono } from "./fonts";
 import { Button, ClientOnly, ContentBlock, ContentHeading } from "./components";
+import { PaStar, PaWordmark } from "@/public/logo";
 import { NOT_FOUND_CONST } from "./const";
 
 export default function NotFound() {
   return (
     <div
-      className={`${hankenGrotesk.variable} ${instrumentSerif.variable} ${dmMono.variable} bg-greyscale-100 dark:bg-greyscale-900 flex h-screen items-center antialiased`}
+      className={`${hankenGrotesk.variable} ${instrumentSerif.variable} ${dmMono.variable} radial-gradient flex h-screen flex-col items-center antialiased`}
     >
       <ClientOnly>
-        <main className="relative mx-auto grid w-full max-w-7xl grid-cols-4 gap-3 px-3 md:grid-cols-8 md:gap-4 md:px-6 lg:grid-cols-12 lg:px-12">
-          <ContentBlock additionalClasses="col-start-2 col-span-2 md:col-start-2 md:col-span-6 lg:col-start-4 lg:col-span-6">
-            <ContentHeading level="h1">not found</ContentHeading>
+        <main className="relative mx-auto grid h-full w-full max-w-7xl grid-cols-4 place-items-center gap-3 px-3 md:grid-cols-8 md:gap-4 md:px-6 lg:grid-cols-12 lg:px-12">
+          <ContentBlock additionalClasses="col-span-full h-fit md:col-start-2 md:col-span-6 lg:col-start-4 lg:col-span-6 -rotate-1">
+            <ContentHeading level="h1">
+              {NOT_FOUND_CONST.HEADING}
+            </ContentHeading>
             <p className="text-greyscale-600 dark:text-greyscale-300 w-full">
               {NOT_FOUND_CONST.DESCRIPTION}
             </p>
@@ -26,6 +29,12 @@ export default function NotFound() {
             </div>
           </ContentBlock>
         </main>
+        <div className="text-greyscale-100 dark:text-greyscale-900 col-span-full mb-3 flex flex-row items-center justify-center gap-2 md:mb-4">
+          <span className="motion-safe:animate-[spin_12s_linear_infinite]">
+            <PaStar />
+          </span>
+          <PaWordmark />
+        </div>
       </ClientOnly>
     </div>
   );
