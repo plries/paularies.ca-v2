cd1836ea4c111b33afd18d4b259ee80a1de8864b
plries <apaulries@gmail.com>
2025-05-20 18:25:33 -0700

    about hero reveal

--- app/(site)/about/components/AboutHero.tsx
+++ app/(site)/about/components/AboutHero.tsx
@@ -1,42 +1,55 @@
 "use client";
 import React from "react";
 import Image from "next/image";
-import { motion } from "framer-motion";
+import { easeInOut, motion } from "framer-motion";
 import { MOTION_CONFIG } from "@/app/(site)/const";
 import { InsetBlock } from "@/app/components";
 import { ABOUT_HERO_CONST } from "../const";
 
 export const AboutHero = () => {
   const imageStyles =
-    "animate-float border-greyscale-100 col-span-2 col-start-2 dark:border-greyscale-900 border shadow-md transition-[scale] duration-500 hover:scale-105 aspect-square rounded-xl";
+    "animate-float border-greyscale-100 col-span-2 col-start-2 dark:border-greyscale-900 border shadow-md transition-[scale] duration-500 hover:scale-105 aspect-square rounded-xl overflow-hidden ";
 
   return (
     <section className="mt-16 contents">
       <InsetBlock>
         <div className="col-span-full flex flex-row items-center justify-center md:col-span-4 md:col-start-3 lg:col-start-5">
-          <Image
-            src={ABOUT_HERO_CONST.IMAGES[0].SRC}
-            alt={ABOUT_HERO_CONST.IMAGES[0].ALT}
-            height={500}
-            width={500}
-            className={`z-10 w-16 translate-x-2 -rotate-4 md:w-36 ${imageStyles}`}
-          />
-          <video
-            autoPlay
-            loop
-            muted
-            playsInline
-            className={`w-24 md:w-48 ${imageStyles}`}
+          <motion.div
+            initial={{ transform: "translateX(100%)" }}
+            animate={{ transform: "translateX(0%)" }}
+            transition={{ duration: 0.7, ease: easeInOut, delay: 0.5 }}
+            className={`z-10 min-w-16 translate-x-2 -rotate-4 md:min-w-36 ${imageStyles}`}
           >
-            <source src={ABOUT_HERO_CONST.VIDEO} type="video/mp4" />
-          </video>
-          <Image
-            src={ABOUT_HERO_CONST.IMAGES[1].SRC}
-            alt={ABOUT_HERO_CONST.IMAGES[1].ALT}
-            height={500}
-            width={500}
-            className={`z-10 w-16 -translate-x-2 rotate-4 md:w-36 ${imageStyles}`}
-          />
+            <Image
+              src={ABOUT_HERO_CONST.IMAGES[0].SRC}
+              alt={ABOUT_HERO_CONST.IMAGES[0].ALT}
+              height={500}
+              width={500}
+            />
+          </motion.div>
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 0.7, ease: easeInOut, delay: 0.7 }}
+            className={`min-w-24 md:min-w-48 ${imageStyles}`}
+          >
+            <video autoPlay loop muted playsInline>
+              <source src={ABOUT_HERO_CONST.VIDEO} type="video/mp4" />
+            </video>
+          </motion.div>
+          <motion.div
+            initial={{ transform: "translateX(-100%)" }}
+            animate={{ transform: "translateX(0%)" }}
+            transition={{ duration: 0.7, ease: easeInOut, delay: 0.5 }}
+            className={`z-10 min-w-16 -translate-x-2 rotate-4 md:min-w-36 ${imageStyles}`}
+          >
+            <Image
+              src={ABOUT_HERO_CONST.IMAGES[1].SRC}
+              alt={ABOUT_HERO_CONST.IMAGES[1].ALT}
+              height={500}
+              width={500}
+            />
+          </motion.div>
         </div>
         <p className="dark:text-greyscale-50 col-span-full flex flex-col items-center gap-1 text-center !text-xl leading-none md:col-span-6 md:col-start-2 md:!text-2xl lg:col-start-4 lg:!text-[1.75rem]">
           <span className="inline-flex w-fit flex-row items-center gap-x-2">
@@ -45,7 +58,7 @@ export const AboutHero = () => {
               height={160}
               src={ABOUT_HERO_CONST.EMOJI.SRC}
               alt={ABOUT_HERO_CONST.EMOJI.ALT}
-              className="aspect-square h-5 w-5 md:h-6 md:w-6 lg:h-7 lg:w-7 ease-in-out-circ transition-transform duration-500 hover:-rotate-12" 
+              className="ease-in-out-circ aspect-square h-5 w-5 transition-transform duration-500 hover:-rotate-12 md:h-6 md:w-6 lg:h-7 lg:w-7"
             />
             {ABOUT_HERO_CONST.HEADING[0]}
           </span>
