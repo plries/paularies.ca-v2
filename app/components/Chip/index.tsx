c477a15cb8f9fd84c988a45d1920becb198e1eee
plries <apaulries@gmail.com>
2025-08-04 22:17:16 -0700

    chip shadow

--- app/components/Chip/index.tsx
+++ app/components/Chip/index.tsx
@@ -1,6 +1,6 @@
 export const Chip = ({ children }: { children: React.ReactNode }) => (
   <>
-    <p className="bg-greyscale-100 font-dm-mono text-greyscale-500 dark:bg-greyscale-900 border-greyscale-950/10 dark:border-greyscale-50/10 dark:text-greyscale-400 rounded-xl border p-2 !text-sm leading-none md:!text-base">
+    <p className="bg-greyscale-100 font-dm-mono text-greyscale-500 dark:bg-greyscale-900 border-greyscale-950/10 dark:border-greyscale-50/10 dark:text-greyscale-400 rounded-xl border p-2 !text-sm leading-none shadow-xs md:!text-base">
       {children}
     </p>
   </>
